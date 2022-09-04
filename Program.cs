2eb509e73c4c5a28ff61ad02afa138f247a50219
fab672000 <fabien@onepost.net>
2022-09-04 18:25:26 -0500

    Initial revision

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿// 
+// Cubase Script generator for Fractal Audio Designs devices
+// 
+// by Fabien (fab672000)
+//
+// GPL License
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AFXPC2CUBASE
+{
+    class Program
+    {
+        private static void SyntaxError(string msg)
+        {
+            Console.WriteLine(msg);
+            Environment.Exit(-1);
+        }
+
+        const string FasDeviceName = "AxeFX3"; // change this for other patches names
+        
+        // Do not change the header below unless you know what you're doing as Cubase is very sensitive to the formatting in the script
+        static readonly string[] HeaderLines = new []
+        {
+            "[cubase parse file]",
+            "[parser version 0001]\r\n",
+            "[creators first name]fab672000",
+            "[creators last name] GM",
+            "[device manufacturer]Fractal Audio Systems",
+            "[device name]Axe FX III",  
+           $"[script name]{FasDeviceName}",
+            "[script version]version 1.00\r\n",
+            "[define patchnames]\r\n",
+        };
+
+        /// <summary>
+        /// This program will generate from an AxeEdit exported preset file a cubase script that can be directly imported to Cubase
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)  SyntaxError(@"Syntax error, expected command is: AFXPC2CUBASE <inputFilePath>\r\n\t Where <inputFilePath> is your AxeEdit exported text file path");
+
+            var inputFile = args[0];
+            var outputFileName = $"{FasDeviceName }.txt";
+            var outputFile = Path.Combine(Path.GetFullPath(Path.GetDirectoryName(inputFile)), outputFileName);
+
+            
+            var lines = File.ReadAllLines(inputFile);
+            var outputLines = new List<string>();
+            foreach (var line in HeaderLines) outputLines.Add(line + "\r\n");
+            foreach (var line in lines)
+            {
+                var input = line.Split('\t').Select(x => x.Trim()).ToArray();
+                if (input.Length<2) continue;
+                if (!int.TryParse(input[0], out int inPcNum)) continue;
+                var bankNum = inPcNum / 128;
+                if (inPcNum % 128 == 0)  outputLines.Add($"[g1]\t\tPatches {inPcNum:0000}-{(inPcNum+127):0000}\r\n");
+                var oLine = $"[p2, {(inPcNum%128)}, {bankNum}, 0]\t{input[1]}\r\n";
+                outputLines.Add(oLine);
+            }
+
+            outputLines.Add("[end]\r\n");
+
+            foreach (var line in outputLines)
+            {
+                Console.WriteLine(line);
+            }
+
+            File.WriteAllLines(outputFile,outputLines.ToArray());
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AFXPC2CUBASE")]
+[assembly: AssemblyDescription("Console tool to convert FAS Axe Edit preset exports into Cubase")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("fab672000 at github.com")]
+[assembly: AssemblyProduct("AFXPC2CUBASE")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30a93e8f-8184-4786-9389-ad42cddebe93")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
